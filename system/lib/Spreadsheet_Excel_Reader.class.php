93bb305382deb5470635d83881c9774e59c5c2cb
wangjian <308541105@qq.com>
2017-04-13 09:36:08 +0800

    2017/4.13

--- system/lib/Spreadsheet_Excel_Reader.class.php
+++ system/lib/Spreadsheet_Excel_Reader.class.php
@@ -841,7 +841,7 @@ class Spreadsheet_Excel_Reader {
 
 		// Custom pattern can be POSITIVE;NEGATIVE;ZERO
 		// The "text" option as 4th parameter is not handled
-		$parts = split(";",$format);
+		$parts = explode(";",$format);
 		$pattern = $parts[0];
 		// Negative pattern
 		if (count($parts)>2 && $num==0) {
