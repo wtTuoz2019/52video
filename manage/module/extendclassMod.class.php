3461a421ac847f8fb4357ee196677c7151c61faf
wangjian <308541105@qq.com>
2017-05-22 09:31:33 +0800

    2017/5.22

--- manage/module/extendclassMod.class.php
+++ manage/module/extendclassMod.class.php
@@ -13,5 +13,14 @@ class extendclassMod extends commonMod {
 		 $this->show();
 		
 		}
+		
+	public function classes(){
+		$this->actionname='班级管理';
+		$this->action='inputs_add';
+		 $fid=intval($_GET['fid']);
+        $this->alert_str($fid,'int');
+		 $this->show();
+		
+		}
 }
 ?>
