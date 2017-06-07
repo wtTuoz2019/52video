56a59c2ad8afe5d7537416a5e2a435267f706863
wangjian <308541105@qq.com>
2017-06-07 13:46:12 +0800

    2017/6.7

---
+++ model/forumModel.class.php
@@ -0,0 +1,25 @@
+<?php
+class forumModel extends commonModel
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+	
+	public function forum_config($where=null){
+		
+		return $this->model->table('forum_config')->where($where)->find();
+		}
+		
+	public function forum_config_save($data){
+		
+		if($this->model->table('forum_config')->where(array('token'=>$data['token']))->find()){
+		
+		return $this->model->table('forum_config')->where(array('token'=>$data['token']))->data($data)->update();
+		
+		}else{
+			$this->model->table('forum_config')->data($data)->insert();	
+			}
+		}
+}
+?>
