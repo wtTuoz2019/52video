8f55112252b5a054002403c73b6e1e0fbe1c71f8
wangjian <308541105@qq.com>
2017-08-17 15:51:23 +0800

    2017/8/17

--- manage/module/extendclassMod.class.php
+++ manage/module/extendclassMod.class.php
@@ -143,28 +143,56 @@ class extendclassMod extends commonMod {
 		  
 		if($_GET['download']){
 			$list=model('extendclass')->teacher_list($where);
+			
+			require(CP_PATH . 'ext/PHPExcel.php');
+			require(CP_PATH . 'ext/PHPExcel/IOFactory.php');
+			$objPHPExcel = new PHPExcel();
+	        $objPHPExcel = new PHPExcel(); 
+      	  $objPHPExcel->getProperties()->setCreator("PHPExcel")
+                                     ->setLastModifiedBy("PHPExcel")
+                                     ->setTitle("PHPExcel reports")
+                                     ->setSubject("PHPExcel reports")
+                                     ->setDescription("PHPExcel document for Office 2003 XLS, generated at ".date('Y-m-d'))
+                                     ->setKeywords("PHPExcel reports")
+                                     ->setCategory("PHPExcel");
+			
+			$keynames=array('名称','手机','职称','师训号','介绍');
+		$keys = array_keys($keynames);
+		 $xlsx[] = $keynames;	
+		foreach($list as $key=>$value){
+		$temp=array( $value['name'], $value['mobile'], $value['title'], $value['SXTEACHNUMBER'], $value['des']);
+		$xlsx[]=$temp;
+	
+      		  
+     	   }	
+		   
+		   $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+1))->setWidth(20);
+			 foreach($xlsx as $index => $row){
+            $i = $index + 1;
+            $sheet = $objPHPExcel->setActiveSheetIndex(0);
+            foreach($keys as $key => $val){
+				if($key<26){
+               $ascii = chr(65+$key);
+				}else{
+				$ascii = chr(65).chr(65+($key-26));	 
+					}
+			$sheet->setCellValueExplicit($ascii.$i, $row[$val],PHPExcel_Cell_DataType::TYPE_STRING);
+              
+			   
+				
+            }
+     
+		
+			}
+			
+        $objPHPExcel->getActiveSheet()->setTitle('老师数据');
+        $objPHPExcel->setActiveSheetIndex(0);ob_end_clean() ;
 		header("Content-Type: text/html; charset=utf-8");
 		header("Content-type:application/vnd.ms-execl");
-		header("Content-Disposition:filename=teacher.xls");
-		echo iconv('utf-8','gbk','名称')."\t";
-		echo iconv('utf-8','gbk','手机')."\t";
-		echo iconv('utf-8','gbk','职称')."\t";
-		echo iconv('utf-8','gbk','师训号')."\t";
-		echo iconv('utf-8','gbk','介绍')."\n";
+		header("Content-Disposition:filename=teacher.xlsx");
+		  $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');  
+            $objWriter->save('php://output');  die;
 		
-	
-			
-			foreach($list as $key=>$value){
-	
-		
-		echo iconv('utf-8','gbk',  $value['name'])."\t";
-		echo iconv('utf-8','gbk',  $value['mobile'])."\t";
-		echo iconv('utf-8','gbk',  $value['title'])."\t";
-		echo iconv('utf-8','gbk',  $value['SXTEACHNUMBER'])."\t";
-		echo iconv('utf-8','gbk',  $value['des']."\n");
-      		  
-     	   }
-		  	die;
 			}
 		
 		
@@ -285,26 +313,60 @@ class extendclassMod extends commonMod {
 		  
 		if($_GET['download']){
 			$list=model('extendclass')->student_list($where);
+				
+			require(CP_PATH . 'ext/PHPExcel.php');
+			require(CP_PATH . 'ext/PHPExcel/IOFactory.php');
+			$objPHPExcel = new PHPExcel();
+	        $objPHPExcel = new PHPExcel(); 
+      	  $objPHPExcel->getProperties()->setCreator("PHPExcel")
+                                     ->setLastModifiedBy("PHPExcel")
+                                     ->setTitle("PHPExcel reports")
+                                     ->setSubject("PHPExcel reports")
+                                     ->setDescription("PHPExcel document for Office 2003 XLS, generated at ".date('Y-m-d'))
+                                     ->setKeywords("PHPExcel reports")
+                                     ->setCategory("PHPExcel");
+			
+			$keynames=array('姓名','联系手机','学籍号','学号','班级');
+		$keys = array_keys($keynames);
+		 $xlsx[] = $keynames;	
+		foreach($list as $key=>$value){
+		$temp=array( $value['name'], $value['mobile'], $value['schoolcode'], $value['codenumber'],  $bj[$value['bj_id']]['grade'].'年级'.$bj[$value['bj_id']]['class'].'班');
+		$xlsx[]=$temp;
+	
+      		  
+     	   }	
+		   
+		   $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+1))->setWidth(20);
+		     $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+2))->setWidth(30);
+			   $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+3))->setWidth(30);
+			 foreach($xlsx as $index => $row){
+            $i = $index + 1;
+            $sheet = $objPHPExcel->setActiveSheetIndex(0);
+            foreach($keys as $key => $val){
+				if($key<26){
+               $ascii = chr(65+$key);
+				}else{
+				$ascii = chr(65).chr(65+($key-26));	 
+					}
+		 $sheet->setCellValueExplicit($ascii.$i, $row[$val],PHPExcel_Cell_DataType::TYPE_STRING);
+               //$sheet->setCellValue($ascii.$i, $row[$val]);
+			   
+				
+            }
+     
+		
+			}
+			
+        $objPHPExcel->getActiveSheet()->setTitle('学生数据');
+        $objPHPExcel->setActiveSheetIndex(0);ob_end_clean() ;
 		header("Content-Type: text/html; charset=utf-8");
 		header("Content-type:application/vnd.ms-execl");
-		header("Content-Disposition:filename=student.xls");
-		echo iconv('utf-8','gbk','名称')."\t";
-		echo iconv('utf-8','gbk','联系手机')."\t";
-		echo iconv('utf-8','gbk','学籍号')."\t";
-		echo iconv('utf-8','gbk','班级')."\n";
-		
-	
+		header("Content-Disposition:filename=student.xlsx");
+		  $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');  
+            $objWriter->save('php://output');  die;
 			
-			foreach($list as $key=>$value){
+			
 	
-		
-		echo iconv('utf-8','gbk',  $value['name'])."\t";
-		echo iconv('utf-8','gbk',  $value['mobile'])."\t";
-		echo iconv('utf-8','gbk',  $value['schoolcode'])."\t";
-		echo iconv('utf-8','gbk',  $bj[$value['bj_id']]['grade'].'年级'.$bj[$value['bj_id']]['class'].'班'."\n");
-      		  
-     	   }
-		  	die;
 			}
 		  
 		   $url = __URL__ . '/student/page-{page}.html';
@@ -447,26 +509,59 @@ class extendclassMod extends commonMod {
 		if($bid){
 		 $list=model('extendclass')->nosignup_course_list($this->user['id'],$bid);	
 		if($list){
-			header("Content-Type: text/html; charset=utf-8");
-		header("Content-type:application/vnd.ms-execl");
-		header("Content-Disposition:filename=signup.xls");
-		echo iconv('utf-8','gbk','姓名')."\t";
-		echo iconv('utf-8','gbk','班级')."\t";
-		echo iconv('utf-8','gbk','联系号码')."\t";
-		echo iconv('utf-8','gbk','学籍号码')."\t";
-		echo iconv('utf-8','gbk','学号')."\t";
-		
-		echo "\n";
-		foreach($list as $k=>$v){
-			echo iconv('utf-8','gbk',$v['name'])."\t";
-			echo iconv('utf-8','gbk',$bj[$v['bj_id']]['grade'].'年级'.$bj[$v['bj_id']]['class'].'班')."\t";
-			echo iconv('utf-8','gbk',$v['mobile'])."\t";
-			echo iconv('utf-8','gbk',$v['schoolcode'])."\t";
 			
-			echo iconv('utf-8','gbk',$v['codenumber'])."\t";
-			echo "\n";
-			}
+				require(CP_PATH . 'ext/PHPExcel.php');
+			require(CP_PATH . 'ext/PHPExcel/IOFactory.php');
+			$objPHPExcel = new PHPExcel();
+	        $objPHPExcel = new PHPExcel(); 
+      	  $objPHPExcel->getProperties()->setCreator("PHPExcel")
+                                     ->setLastModifiedBy("PHPExcel")
+                                     ->setTitle("PHPExcel reports")
+                                     ->setSubject("PHPExcel reports")
+                                     ->setDescription("PHPExcel document for Office 2003 XLS, generated at ".date('Y-m-d'))
+                                     ->setKeywords("PHPExcel reports")
+                                     ->setCategory("PHPExcel");
+			$keynames=array('姓名','联系手机','学籍号','学号','班级');
+		$keys = array_keys($keynames);
+		 $xlsx[] = $keynames;	
+		foreach($list as $key=>$value){
+		$temp=array( $value['name'], $value['mobile'], $value['schoolcode'], $value['codenumber'],  $bj[$value['bj_id']]['grade'].'年级'.$bj[$value['bj_id']]['class'].'班');
+		$xlsx[]=$temp;
+	
+      		  
+     	   }	
+		   
+		   $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+1))->setWidth(20);
+		     $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+2))->setWidth(30);
+			   $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+3))->setWidth(30);
+			 foreach($xlsx as $index => $row){
+            $i = $index + 1;
+            $sheet = $objPHPExcel->setActiveSheetIndex(0);
+            foreach($keys as $key => $val){
+				if($key<26){
+               $ascii = chr(65+$key);
+				}else{
+				$ascii = chr(65).chr(65+($key-26));	 
+					}
+		 $sheet->setCellValueExplicit($ascii.$i, $row[$val],PHPExcel_Cell_DataType::TYPE_STRING);
+               //$sheet->setCellValue($ascii.$i, $row[$val]);
+			   
+				
+            }
+     
 		
+			}
+			
+        $objPHPExcel->getActiveSheet()->setTitle('学生数据');
+        $objPHPExcel->setActiveSheetIndex(0);ob_end_clean() ;
+		header("Content-Type: text/html; charset=utf-8");
+		header("Content-type:application/vnd.ms-execl");
+		header("Content-Disposition:filename=student.xlsx");
+		  $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');  
+            $objWriter->save('php://output');  die;
+			
+			
+	
 			
 			
 			
@@ -484,33 +579,55 @@ class extendclassMod extends commonMod {
 		if($_POST['bj_id']){
 		 $list=model('extendclass')->signup_course_list('A.uid='.$this->user['id'].' and A.bj_id='.$_POST['bj_id']);	
 		if($list){
-			header("Content-Type: text/html; charset=utf-8");
-		header("Content-type:application/vnd.ms-execl");
-		header("Content-Disposition:filename=signup.xls");
-		echo iconv('utf-8','gbk','姓名')."\t";
-		echo iconv('utf-8','gbk','班级')."\t";
-		echo iconv('utf-8','gbk','联系号码')."\t";
-		echo iconv('utf-8','gbk','学籍号码')."\t";
-		echo iconv('utf-8','gbk','学号')."\t";
-		echo iconv('utf-8','gbk','课程名称')."\t";
-		echo iconv('utf-8','gbk','任课老师')."\t";
-		echo iconv('utf-8','gbk','分数')."\t";
-		echo "\n";
-		foreach($list as $k=>$v){
-			echo iconv('utf-8','gbk',$v['name'])."\t";
-			echo iconv('utf-8','gbk',$bj[$v['bj_id']]['grade'].'年级'.$bj[$v['bj_id']]['class'].'班')."\t";
-			echo iconv('utf-8','gbk',$v['mobile'])."\t";
-			echo iconv('utf-8','gbk',$v['schoolcode'])."\t";
-			
-			echo iconv('utf-8','gbk',$v['codenumber'])."\t";
-			echo iconv('utf-8','gbk',$v['title'])."\t";
-			echo iconv('utf-8','gbk',$teacher[$v['tid']]['name'])."\t";
-			echo iconv('utf-8','gbk',$v['score'])."\t";
-			echo "\n";
-			}
+			require(CP_PATH . 'ext/PHPExcel.php');
+			require(CP_PATH . 'ext/PHPExcel/IOFactory.php');
+			$objPHPExcel = new PHPExcel();
+	        $objPHPExcel = new PHPExcel(); 
+      	  $objPHPExcel->getProperties()->setCreator("PHPExcel")
+                                     ->setLastModifiedBy("PHPExcel")
+                                     ->setTitle("PHPExcel reports")
+                                     ->setSubject("PHPExcel reports")
+                                     ->setDescription("PHPExcel document for Office 2003 XLS, generated at ".date('Y-m-d'))
+                                     ->setKeywords("PHPExcel reports")
+                                     ->setCategory("PHPExcel");
+			$keynames=array('姓名','联系手机','学籍号','学号','班级','课程名称','任课老师','分数');
+		$keys = array_keys($keynames);
+		 $xlsx[] = $keynames;	
+		foreach($list as $key=>$value){
+		$temp=array( $value['name'], $value['mobile'], $value['schoolcode'], $value['codenumber'],  $bj[$value['bj_id']]['grade'].'年级'.$bj[$value['bj_id']]['class'].'班',$value['title'],$teacher[$value['tid']]['name'],$value['score']);
+		$xlsx[]=$temp;
+	
+      		  
+     	   }	
+		   
+		   $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+1))->setWidth(20);
+		     $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+2))->setWidth(30);
+			   $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+3))->setWidth(30);
+			 foreach($xlsx as $index => $row){
+            $i = $index + 1;
+            $sheet = $objPHPExcel->setActiveSheetIndex(0);
+            foreach($keys as $key => $val){
+				if($key<26){
+               $ascii = chr(65+$key);
+				}else{
+				$ascii = chr(65).chr(65+($key-26));	 
+					}
+		 $sheet->setCellValueExplicit($ascii.$i, $row[$val],PHPExcel_Cell_DataType::TYPE_STRING);
+               //$sheet->setCellValue($ascii.$i, $row[$val]);
+			   
+				
+            }
+     
 		
+			}
 			
-			
+        $objPHPExcel->getActiveSheet()->setTitle('学生数据');
+        $objPHPExcel->setActiveSheetIndex(0);ob_end_clean() ;
+		header("Content-Type: text/html; charset=utf-8");
+		header("Content-type:application/vnd.ms-execl");
+		header("Content-Disposition:filename=student.xlsx");
+		  $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');  
+            $objWriter->save('php://output');  
 			
 			
 			}	
@@ -606,30 +723,55 @@ class extendclassMod extends commonMod {
 		 $list=model('extendclass')->signup_course_list('A.uid='.$this->user['id'].' and B.cid='.$cid);	
 		if($list){
 		 $this->teacher=$teacher=model('extendclass')->teacher_list($where);
-				header("Content-Type: text/html; charset=utf-8");
-		header("Content-type:application/vnd.ms-execl");
-		header("Content-Disposition:filename=signup.xls");
-		echo iconv('utf-8','gbk','姓名')."\t";
-		echo iconv('utf-8','gbk','学籍号')."\t";
-		echo iconv('utf-8','gbk','学号')."\t";
-		echo iconv('utf-8','gbk','班级')."\t";
-		echo iconv('utf-8','gbk','联系号码')."\t";
-		echo iconv('utf-8','gbk','课程名称')."\t";
-		echo iconv('utf-8','gbk','任课老师')."\t";
-		echo iconv('utf-8','gbk','分数')."\t";
-		echo "\n";
-		foreach($list as $k=>$v){
-			echo iconv('utf-8','gbk',$v['name'])."\t";
-			echo iconv('utf-8','gbk',$v['schoolcode'])."\t";
-			echo iconv('utf-8','gbk',$v['codenumber'])."\t";
-			echo iconv('utf-8','gbk',$bj[$v['bj_id']]['grade'].'年级'.$bj[$v['bj_id']]['class'].'班')."\t";
-			echo iconv('utf-8','gbk',$v['mobile'])."\t";
-			echo iconv('utf-8','gbk',$v['title'])."\t";
-			echo iconv('utf-8','gbk',$teacher[$v['tid']]['name'])."\t";
-			echo iconv('utf-8','gbk',$v['score'])."\t";
-			echo "\n";
-			}
+				require(CP_PATH . 'ext/PHPExcel.php');
+			require(CP_PATH . 'ext/PHPExcel/IOFactory.php');
+			$objPHPExcel = new PHPExcel();
+	        $objPHPExcel = new PHPExcel(); 
+      	  $objPHPExcel->getProperties()->setCreator("PHPExcel")
+                                     ->setLastModifiedBy("PHPExcel")
+                                     ->setTitle("PHPExcel reports")
+                                     ->setSubject("PHPExcel reports")
+                                     ->setDescription("PHPExcel document for Office 2003 XLS, generated at ".date('Y-m-d'))
+                                     ->setKeywords("PHPExcel reports")
+                                     ->setCategory("PHPExcel");
+			$keynames=array('姓名','联系手机','学籍号','学号','班级','课程名称','任课老师','分数');
+		$keys = array_keys($keynames);
+		 $xlsx[] = $keynames;	
+		foreach($list as $key=>$value){
+		$temp=array( $value['name'], $value['mobile'], $value['schoolcode'], $value['codenumber'],  $bj[$value['bj_id']]['grade'].'年级'.$bj[$value['bj_id']]['class'].'班',$value['title'],$teacher[$value['tid']]['name'],$value['score']);
+		$xlsx[]=$temp;
+	
+      		  
+     	   }	
+		   
+		   $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+1))->setWidth(20);
+		     $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+2))->setWidth(30);
+			   $objPHPExcel->getActiveSheet()->getColumnDimension( chr(65+3))->setWidth(30);
+			 foreach($xlsx as $index => $row){
+            $i = $index + 1;
+            $sheet = $objPHPExcel->setActiveSheetIndex(0);
+            foreach($keys as $key => $val){
+				if($key<26){
+               $ascii = chr(65+$key);
+				}else{
+				$ascii = chr(65).chr(65+($key-26));	 
+					}
+		 $sheet->setCellValueExplicit($ascii.$i, $row[$val],PHPExcel_Cell_DataType::TYPE_STRING);
+               //$sheet->setCellValue($ascii.$i, $row[$val]);
+			   
+				
+            }
+     
 		
+			}
+			
+        $objPHPExcel->getActiveSheet()->setTitle('学生数据');
+        $objPHPExcel->setActiveSheetIndex(0);ob_end_clean() ;
+		header("Content-Type: text/html; charset=utf-8");
+		header("Content-type:application/vnd.ms-execl");
+		header("Content-Disposition:filename=student.xlsx");
+		  $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');  
+            $objWriter->save('php://output');  
 			
 			
 			
