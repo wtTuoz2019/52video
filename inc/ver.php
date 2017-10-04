9687d3db07a493615ec082bcffbace735c272b21
wangjian <308541105@qq.com>
2017-10-04 19:54:59 +0800

    2017/9/28

---
+++ inc/ver.php
@@ -0,0 +1,41 @@
+<?php
+$config['ver'] = '1.1.1'; //版本号
+$config['ver_name'] = ' 稳定版'; //版本名称
+$config['ver_date'] = ''; //版本时间
+$config['appid'] = 'wxa0f10e67e5a297e2'; //微信appid
+$config['appsecret'] = '5e744b2bc549f3e05af1e56fb1999838';
+$config['loginappid'] = 'wx39b5c46ac251820b'; //微信appid
+$config['loginappsecret'] = 'b0f6311e8b9a3b9016737360dfa19728';
+$config['kfappid'] = 'wxa31b78e4346ef904'; //微信appid
+$config['kfappsecret'] = '551951fda6beeb0f1c7974701f0a9a6f';
+$config['kftoken'] = 'Shanyueyun';
+$config['kfkey'] = 'ShanyueyunShanyueyunShanyueyunShanyueyun123';
+$config['play']="play.heims.com.cn/schoolvideo"; 
+$config['ios']="ios.heims.com.cn/schoolvideo"; 
+$config['imageurl']="http://image.shanyueyun.com"; 
+$config['hk']="http://hk.urren.net/"; 
+$config['out']="play.shanyueyun.com"; 
+$config['ali']="live.urren.net/shanyueyun/"; 
+$config['jsrtmp']="urren.rtmplive.ks-cdn.com/live/"; 
+$config['jshls']="urren.hlslive.ks-cdn.com/live/"; 
+$config['jsvideo']="http://uv.urren.net/"; 
+$config['DomainName']="live.urren.net"; 
+$config['AppName']="shanyueyun"; 
+$config['OssEndpoint']='oss-cn-hangzhou.aliyuncs.com';
+$config['OssBucket']='shanyueyunin';
+$config['addurl']="http://mp.weixin.qq.com/s?__biz=MzI1ODE1NTA3MA==&mid=401655815&idx=1&sn=12e50037d8e83fa8dd53c71bffed53c2&scene=0&previewkey=%2FHNoLEJ26gPbpUTkR5Hxi8NS9bJajjJKzz%2F0By7ITJA%3D#rd"; 
+$config['AccessID']="968436971281"; 
+$config['AccessKEY']="LeW0uX5NDHT5VqNJ5d8s6OeSRIh6PZRo"; 
+$config['subscribeid']="bKqF81nbVY_doafWcEWKmgB8Uq00D6QL7dXCWhs5GQw"; 
+$config['AccessKeyId']="UakpFlsrvQ9Jdp9q";  
+$config['AccessSecret']="l2tWGS36dH5F8F9hICO7QbclIGlNYN"; 
+$config['types']=array('video'=>array('value'=>'video','name'=>'视频'),
+					   'file'=>array('value'=>'file','name'=>'文件资源'),
+					// 'paper'=>array('value'=>'paper','name'=>'试卷')
+					   	); 
+$config['fujia']=array('video'=>array('url'=>'/video','type'=>'video','name'=>'视频'),
+					   'videos'=>array('url'=>'/video','type'=>'videos','name'=>'视频列表'),
+					   'file'=>array('url'=>'/source','type'=>'file','name'=>'文档上传'),
+					   'files'=>array('url'=>'/source','type'=>'files','name'=>'文档资源'),
+					  );
+?>
