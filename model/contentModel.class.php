562e8407bb4d3aecb78b152d544dcb4ea27939a4
wangjian <308541105@qq.com>
2017-10-10 21:01:55 +0800

    2017/10/10

--- model/contentModel.class.php
+++ model/contentModel.class.php
@@ -538,7 +538,7 @@ class contentModel extends commonMod
     }
 	public function video_info($where)
     {	
-		 $id=$this->model->field('id,name,vurl,type,zize')->table('videolist')->where($where)->find(); 
+		 $id=$this->model->field('id,name,vurl,type,size')->table('videolist')->where($where)->find(); 
       
         return $id;
     }
